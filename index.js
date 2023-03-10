0568e7011b8dcf6de30ec73c58680ae11e2f8fe8
Nilay Varshney <nyvy.07@gmail.com>
2023-03-10 23:06:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -9,28 +9,53 @@ div {
 img{
   object-fit: contain;
   width: 100%;
-  height: 100%
+  height: 100%;
 }
 span {
   position: absolute;
-  width: 35%;
-  top: 59%;
-  left: 15.2%;
+  width: 20%;
+  top: 72.5%;
+  left: 30%;
   height: 2%;
   border-radius: 50% 0% 0% 50%;
   background-color: grey;
-  transition: all 1s linear;
   transform-origin: 100%;
 }
+
+// span:after {
+//   display: block;
+//   content:'';
+//   width: 20px;
+//   height: 20px;
+//   color: black;
+//   background: black;
+//   z-index: 100;
+//   position: absolute;
+//   top: -6px;
+// }
+
+// span:before {
+//   display: block;
+//   content: '';
+//   width: 20px;
+//   height: 20px;
+//   color: black;
+//   position: absolute;
+//   right: 0;
+//   top: -6px;
+//   background: black;
+//   z-index: 100;
+//   border-radius: 50%;
+//   outline: 10px solid red;
+//   border : 5px solid white;
+// }
 </style>`;
 
 const template = document.createElement("template");
 template.innerHTML = `
     ${styles}
     <div>
-    <img
-      src="https://www.bajajallianzlife.com/content/dam/balic/index/BMI-Calculator.png"
-    />
+    <img src="./bmi.png"/>
     <span></span>
   </div>
 `;
@@ -58,10 +83,17 @@ class BMIMeter extends HTMLElement {
   getNeedleAngle(value) {
     const numValue = Number(value);
     if (!isNaN(numValue) && numValue >= 0) {
-      if (numValue >= 45) {
-        return 180;
+      if (numValue < 18.5) {
+        return 15;
+      } else if (numValue >= 18.5 && numValue < 25) {
+        return 52;
+      } else if (numValue >= 25 && numValue < 30) {
+        return 90;
+      } else if (numValue >= 30 && numValue < 35) {
+        return 129;
+      } else if (numValue > 35) {
+        return 165;
       }
-      return 4 * numValue;
     } else {
       return 0;
     }
@@ -70,14 +102,16 @@ class BMIMeter extends HTMLElement {
   getNeedleColor(value) {
     const numValue = Number(value);
     if (!isNaN(numValue)) {
-      if (numValue >= 0 && numValue < 10) {
-        return "darkorange";
-      } else if (numValue >= 10 && numValue < 23) {
-        return "green";
-      } else if (numValue >= 23 && numValue < 36) {
-        return "chocolate";
-      } else if (numValue >= 36) {
-        return "red";
+      if (numValue >= 0 && numValue < 18.5) {
+        return "#18aee2";
+      } else if (numValue >= 18.5 && numValue < 25) {
+        return "#41b75a";
+      } else if (numValue >= 25 && numValue < 30) {
+        return "#efeb42";
+      } else if (numValue >= 30 && numValue < 35) {
+        return "#f79633";
+      } else if (numValue > 35) {
+        return "#ee3138";
       }
     } else {
       return "grey";
